96c118d2c5b1a7b617550c2f78b316fbd4ed258d
MuhittinBasarErsoy <basarersoy@gmail.com>
2023-01-19 11:29:49 +0300

    First API Endpoint - HTTPGET

---
+++ MagicVilla_VillaAPI/Models/Villa.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc.ApplicationModels;
+
+namespace MagicVilla_VillaAPI.Models
+{
+    public class Villa
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
